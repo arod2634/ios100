3efceba70d329d4d655130dffd242dd16425d72c
Alex Rodriguez <arod@integritystl.com>
2014-03-02 20:29:19 -0600

    Created and populated an NSMutableArray and logged the values three different ways

--- randomItems/randomItems/main.m
+++ randomItems/randomItems/main.m
@@ -13,8 +13,34 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        // Create a muteable array object, store its address in items variable
+        NSMutableArray *items = [[NSMutableArray alloc] init];
+        
+        // Send the message addObject: to the NSMutableArray pointed to by the variable items, passing one string at a time
+        [items addObject:@"One"];
+        [items addObject:@"Two"];
+        [items addObject:@"Three"];
+        
+        // Send another message, insertObjectAtIndex:, to that same array object
+        [items insertObject:@"Zero" atIndex:0];
+        
+        // Do a traditional for loop though each item of the array
+        for (int i = 0 ; i < [items count]; i++) {
+            NSString *item = items[i]; // You could also use NSString *item = [items objectAtIndex:i];
+            NSLog(@"%@", item);
+        }
+        
+        // Use fast enumeration to loop through the array
+        // For every item in the items array...
+        for (NSString *item in items)
+            NSLog(@"%@",item);
+        
+        
+        // Log out the values of the items object
+        NSLog(@"%@", items);
+        
+        // Destroy the mutable array object since we no longer need it
+        items = nil;
         
     }
     return 0;
