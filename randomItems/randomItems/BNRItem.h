3b507e3c4bf687b5ba17c183e81d5ee0ad516721
Alex Rodriguez <arod@integritystl.com>
2014-03-02 21:12:08 -0600

    p43 Added ivars and getter/setter methods for each ivar

---
+++ randomItems/randomItems/BNRItem.h
@@ -0,0 +1,32 @@
+//
+//  BNRItem.h
+//  randomItems
+//
+//  Created by Alex Rodriguez on 3/2/14.
+//  Copyright (c) 2014 Alex Rodriguez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRItem : NSObject
+{
+    // Add instance variables to class
+    NSString *itemName;
+    NSString *serialNumber;
+    int valueInDollars;
+    NSDate *dateCreated;
+}
+
+// Declare the getter and setter methods for each instance variable
+- (void)setItemName:(NSString *)str;
+- (NSString *)itemName;
+
+- (void)setSerialNumber:(NSString *)str;
+- (NSString *)serialNumber;
+
+- (void)setValueInDollars:(NSString *)str;
+- (NSString *)valueInDollars;
+
+- (NSDate *)dateCreated;
+
+@end
---
+++ randomItems/randomItems/BNRItem.m
@@ -0,0 +1,13 @@
+//
+//  BNRItem.m
+//  randomItems
+//
+//  Created by Alex Rodriguez on 3/2/14.
+//  Copyright (c) 2014 Alex Rodriguez. All rights reserved.
+//
+
+#import "BNRItem.h"
+
+@implementation BNRItem
+
+@end
