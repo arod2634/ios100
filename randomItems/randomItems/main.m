97f54e4a65478c7fe96df58063e1ff2cde2599de
Alex Rodriguez <arod@integritystl.com>
2014-03-02 18:23:10 -0600

    Created empty command line tool

---
+++ randomItems/randomItems/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  randomItems
+//
+//  Created by Alex Rodriguez on 3/2/14.
+//  Copyright (c) 2014 Alex Rodriguez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
